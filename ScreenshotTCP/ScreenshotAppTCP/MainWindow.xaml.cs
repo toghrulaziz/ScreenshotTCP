b76b9cbd51e3c38c39951c50d5f8382af4f03618
toghrulaziz <togrul1609@gmail.com>
2023-08-07 23:20:01 +0400

    Screenshot TCP

---
+++ ScreenshotTCP/ScreenshotAppTCP/MainWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ScreenshotAppTCP
+{
+    
+    public partial class MainWindow : Window
+    {
+        public BitmapImage PhotoSource
+        {
+            get { return (BitmapImage)GetValue(PhotoSourceProperty); }
+            set { SetValue(PhotoSourceProperty, value); }
+        }
+
+
+        public static readonly DependencyProperty PhotoSourceProperty =
+            DependencyProperty.Register("PhotoSource", typeof(BitmapImage), typeof(MainWindow));
+
+
+        public TcpClient Client { get; set; }
+        public int Port { get; set; }
+        public IPAddress IpAddress { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            Port = 27001;
+            IpAddress = IPAddress.Parse("192.168.0.104");
+        }
+
+
+        public RelayCommand StartCommand
+        {
+            get => new RelayCommand(() =>
+            {
+
+                Task.Factory.StartNew(() =>
+                {
+                    try
+                    {
+                        Client = new();
+                        Client.Connect(IpAddress, Port);
+                        while (true)
+                        {
+                            if (Client.Connected)
+                            {
+                                try
+                                {
+                                    using (NetworkStream networkStream = Client.GetStream())
+                                    {
+
+                                        byte[] imageData = new byte[4096];
+                                        int bytesRead;
+                                        using (MemoryStream memoryStream = new MemoryStream())
+                                        {
+
+                                            while ((bytesRead = networkStream.Read(imageData, 0, imageData.Length)) > 0)
+                                            {
+                                                memoryStream.Write(imageData, 0, bytesRead);
+                                            }
+
+                                            memoryStream.Seek(0, SeekOrigin.Begin);
+                                            BitmapImage bitmapImage = new BitmapImage();
+                                            bitmapImage.BeginInit();
+                                            bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                                            bitmapImage.StreamSource = memoryStream;
+                                            bitmapImage.EndInit();
+                                            bitmapImage.Freeze();
+
+                                            Dispatcher.Invoke(() => PhotoSource = bitmapImage);
+
+                                        }
+
+                                    }
+
+                                }
+                                catch (Exception ex)
+                                {
+                                    MessageBox.Show("Error loading the image: " + ex.Message);
+                                }
+
+                            }
+
+                        }
+
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }, TaskCreationOptions.LongRunning);
+
+            });
+        }
+
+
+
+    }
+}
---
+++ ScreenshotTCP/Server/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Drawing;
+using System.Net;
+using System.Net.Sockets;
+
+var ipAdress = IPAddress.Parse("192.168.0.104");
+var port = 27001;
+
+TcpListener listener = new(ipAdress, port);
+
+listener.Start();
+
+Console.WriteLine($"Listening at {listener.LocalEndpoint}...");
+
+
+while (true)
+{
+    var client = listener.AcceptTcpClient();
+
+    Console.WriteLine($"{client.Client.RemoteEndPoint} connected...");
+
+    Task.Run(() =>
+    {
+        while (true)
+        {
+            var stream = client.GetStream();
+            var image = ScreenShot();
+
+            ImageConverter imageConverter = new();
+
+            var bytes = (byte[])imageConverter.ConvertTo(image, typeof(byte[]))!;
+            stream.Write(bytes);
+            Console.WriteLine("Screenshoted!");
+
+            Console.WriteLine($"{bytes.Length} Sent");
+
+            stream.Close();
+
+            Task.Delay(5000).Wait();
+        }
+
+    });
+
+
+}
+
+
+Bitmap ScreenShot()
+{
+    Bitmap memoryImage;
+    memoryImage = new Bitmap(1920, 1080);
+    Size s = new Size(memoryImage.Width, memoryImage.Height);
+
+    Graphics memoryGraphics = Graphics.FromImage(memoryImage);
+
+    memoryGraphics.CopyFromScreen(0, 0, 0, 0, s);
+
+    return memoryImage;
+}
+
